8f304952bfc032b5698489152f8885963a8e5bd9
hilawnabangus <jaymaryugo90@gmail.com>
2025-02-28 10:49:45 +0800

    found some reference code from pedromussolin, study and learn from it. thanks

--- Flask_Restful/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_restful import Api
- 
-app = Flask(__name__)
- 
-api = Api(app) #Flask REST Api code 
- 
-if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
