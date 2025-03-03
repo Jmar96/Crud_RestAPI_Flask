61c6e375d38e6e17b6f1233596df1c06d9dd461c
hilawnabangus <jaymaryugo90@gmail.com>
2025-03-03 15:23:47 +0800

    will fix this later

--- BadyetBuddy_prototype/app/routes.py
+++ BadyetBuddy_prototype/app/routes.py
@@ -1,29 +1,23 @@
-from flask import Blueprint, jsonify, request, abort,render_template
+from flask import Blueprint, jsonify, render_template
 from .models import Badyet_Items
 from . import db
-
-import os
-
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    print("Current working directory:", os.getcwd())
     return render_template('index.html')
 
-
-# GET ALL USERS - GET
 @main.route('/items', methods=['GET'])
-def get_users():
+def get_items():
     b_items = Badyet_Items.query.all()
     return jsonify([
         {
-            'id': u.id, 
-            'name': u.name, 
-            'description': u.description, 
-            'category': u.category, 
-            'length': u.length, 
-            'amount': u.amount
-        } for u in b_items
+            'id': item.id, 
+            'name': item.name, 
+            'description': item.description, 
+            'category': item.category, 
+            'length': item.length, 
+            'amount': item.amount
+        } for item in b_items
     ])
