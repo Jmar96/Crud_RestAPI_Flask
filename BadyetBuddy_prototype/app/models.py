d21421b0bed099ea6ff9df509b3833b745db02e5
hilawnabangus <jaymaryugo90@gmail.com>
2025-03-04 16:34:30 +0800

    update

--- BadyetBuddy_prototype/app/models.py
+++ BadyetBuddy_prototype/app/models.py
@@ -1,9 +1,21 @@
 from . import db
+from datetime import datetime
 
 class Badyet_Items(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(120))
     description = db.Column(db.String(200))
     category = db.Column(db.String(90))
     length = db.Column(db.String(90))
     amount = db.Column(db.Float)
+    owner_id = db.Column(db.Integer)
+
+class Badyet_Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(120))
+    email = db.Column(db.String(200))
+    password = db.Column(db.String(120))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+
+
--- BadyetBuddy_prototype/app/routes.py
+++ BadyetBuddy_prototype/app/routes.py
@@ -6,12 +6,11 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return render_template('index.html')
-
+    return render_template('index.html', page="home")
 
 @main.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', page="about")
 
 @main.route('/items', methods=['GET'])
 def get_items():
@@ -23,6 +22,55 @@ def get_items():
             'description': item.description, 
             'category': item.category, 
             'length': item.length, 
-            'amount': item.amount
+            'amount': item.amount,
+            'owner_id': item.owner_id
         } for item in b_items
     ])
+
+@main.route('/dashboard')
+def dashboard():
+    b_items = Badyet_Items.query.all()
+    items = [
+        {
+            'id': item.id,
+            'name': item.name,
+            'description': item.description,
+            'category': item.category,
+            'length': item.length,
+            'amount': item.amount,
+            'owner_id': item.owner_id
+        } for item in b_items
+    ]
+    return render_template('dashboard.html', items=items, page="dashb")
+
+@main.route('/incomes')
+def incomes():
+    b_items = Badyet_Items.query.filter_by(category='income').all()
+    incomes = [
+        {
+            'id': item.id,
+            'name': item.name,
+            'description': item.description,
+            'category': item.category,
+            'length': item.length,
+            'amount': item.amount,
+            'owner_id': item.owner_id
+        } for item in b_items
+    ]
+    return render_template('incomes.html', incomes=incomes)
+
+@main.route('/expenses')
+def expenses():
+    b_items = Badyet_Items.query.filter_by(category='expense').all()
+    expenses = [
+        {
+            'id': item.id,
+            'name': item.name,
+            'description': item.description,
+            'category': item.category,
+            'length': item.length,
+            'amount': item.amount,
+            'owner_id': item.owner_id
+        } for item in b_items
+    ]
+    return render_template('expenses.html', expenses=expenses)
